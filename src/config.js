25c55b7968b2dd54149a7c631c035d69c8b06dac
unknown <jguillermolaura@gmail.com>
2022-07-22 22:35:59 -0400

    first commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/apimigrañas",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb+srv://guille:8301405gl@cluster0.gaubm.mongodb.net/?retryWrites=true&w=majority",
   PORT: process.env.PORT || 4000,
   SECRET: 'products-api'
 };
